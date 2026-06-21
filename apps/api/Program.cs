d5c2c4791c144d0c4b417e9c5170e5193fdbf399
brendanball4 <ball.brendan10@gmail.com>
2026-06-20 23:19:54 -0600

    feat(api): register WebhookCityDbContext with Npgsql

--- apps/api/Program.cs
+++ apps/api/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebhookCity.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<WebhookCityDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
